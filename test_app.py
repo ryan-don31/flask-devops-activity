e1b1a713c11281d4589bd3ecb774ab460be336e2
Ryan Don <rjdon9@gmail.com>
2025-11-17 13:05:55 -0500

    Initial commit

---
+++ test_app.py
@@ -0,0 +1,8 @@
+# test_app.py
+import app
+
+def test_hello():
+    client = app.app.test_client()
+    response = client.get('/hello')
+    assert response.status_code == 200
+    assert response.get_json() == {"message": "Hello, World!"}
